77bcf05c420721d6b1108479fb25580768e0d71d
Zachary Kaufman <zckaufman@Zacharys-MacBook-Pro-4.local>
2024-02-26 19:00:41 -0500

    Initial scripts

---
+++ scripts/maryhadalittlelamb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import rospy
+from haru_drums_ros_driver.msg import DrumMidiSignal
+
+pos = 0
+score = 0
+mhall_seq = ["yellow", "orange", "red", "orange", "yellow", "yellow", "yellow", "orange",
+            "orange", "orange", "yellow", "blue", "blue"]
+
+def drum_callback(sig):
+    global pos, score, mhall_seq
+    if(sig.delta_time > 1.0):
+        print("Oh no! You took too long to hit the drum. Let's try again!")
+        return
+    if(sig.color == mhall_seq[pos]):
+        print("Great!")
+        score += 1
+        pos += 1
+    else:
+        print("Oh no! You hit the wrong drum. Let's try again!")
+    
+
+
+if __name__ == "__main__":
+    rospy.init_node("mhall_node", anonymous=True)
+
+    sub = rospy.Subscriber(f"replay/00/hit", DrumMidiSignal, drum_callback)
